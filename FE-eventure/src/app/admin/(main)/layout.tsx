892873e24b162fd1d59ef6fd58575ab92eadaf8c
gutemonmon052 <herryliukae052@gmail.com>
2025-02-13 12:53:24 +0800

    feat: admin sidebar

---
+++ FE-eventure/src/app/admin/(main)/layout.tsx
@@ -0,0 +1,39 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import Archivo from "next/font/local";
+import "../../globals.css";
+import { AuthContexts } from "@/components/contexts/AuthContexts";
+import { Sidebar } from "@/components/admin/sidebar";
+
+const archivoBold = Archivo({
+  src: "../../../../public/assets/fonts/Archivo/static/Archivo-Bold.ttf",
+  variable: "--font-archivo-bold",
+});
+
+const archivoRegular = Archivo({
+  src: "../../../../public/assets/fonts/Archivo/static/Archivo-Regular.ttf",
+  variable: "--font-archivo-regular",
+});
+
+export const metadata: Metadata = {
+  title: "Admin Eventure",
+  description:
+    "Eventure is the best platform to find and purchase tickets for your favorite events. From music concerts, festivals, seminars, to sports-all in one place! Enjoy easy transactions, digital ticket access, and an unforgettable event experience.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${archivoBold.variable} ${archivoRegular.variable} antialiased`}
+      >
+        <Sidebar/>
+        <AuthContexts>{children}</AuthContexts>
+      </body>
+    </html>
+  );
+}
---
+++ FE-eventure/src/app/admin/(main)/page.tsx
@@ -0,0 +1,14 @@
+import '@/css/adminPage/dashboardPage/dashboardPage.css';
+
+export default function Dashboard(){
+    return(
+        <div className="dashboard">
+            <div className="dashboard-title">
+                <span>Welcome</span>
+            </div>
+            <div className="dashboard-content">
+                
+            </div>
+        </div>
+    )
+}
